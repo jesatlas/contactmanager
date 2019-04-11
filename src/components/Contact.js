fa74a973d0b0222483206c2ac979d97594074586
jesatlas <knoblauchjesse@gmail.com>
2019-04-11 08:22:57 -0500

    add reducer/dispatch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,34 +7,45 @@ class Contact extends Component {
     showDetails: false
   };
 
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  };
+
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{' '}
-          <i
-            onClick={() =>
-              this.setState({ showDetails: !this.state.showDetails })
-            }
-            className={this.state.showDetails ? 'fas fa-minus' : 'fas fa-plus'}
-            style={{ cursor: 'pointer' }}
-          />
-          <i
-            onClick={() => {
-              console.log('delete button');
-            }}
-            className="fas fa-times"
-            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-          />
-        </h4>
-        {this.state.showDetails ? (
-          <ul className="list-group">
-            <li className="list-group-item">email: {email}</li>
-            <li className="list-group-item">phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={() =>
+                    this.setState({ showDetails: !this.state.showDetails })
+                  }
+                  className={
+                    this.state.showDetails ? 'fas fa-minus' : 'fas fa-plus'
+                  }
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                />
+              </h4>
+              {this.state.showDetails ? (
+                <ul className="list-group">
+                  <li className="list-group-item">email: {email}</li>
+                  <li className="list-group-item">phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
