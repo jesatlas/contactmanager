f688bcde16f1aea85a0f08c81d8612f7d7d1bdd8
jesatlas <knoblauchjesse@gmail.com>
2019-04-11 07:42:32 -0500

    add context api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 
+import { Consumer } from '../context';
+
 class Contact extends Component {
+  state = {
+    showDetails: false
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">email: {email}</li>
-          <li className="list-group-item">phone: {phone}</li>
-        </ul>
+        <h4>
+          {name}{' '}
+          <i
+            onClick={() =>
+              this.setState({ showDetails: !this.state.showDetails })
+            }
+            className={this.state.showDetails ? 'fas fa-minus' : 'fas fa-plus'}
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            onClick={() => {
+              console.log('delete button');
+            }}
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+          />
+        </h4>
+        {this.state.showDetails ? (
+          <ul className="list-group">
+            <li className="list-group-item">email: {email}</li>
+            <li className="list-group-item">phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 
-class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '222-222-2222'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '111-111-1111'
-      }
-    ]
-  };
-  render() {
-    const { contacts } = this.state;
+import { Consumer } from '../context';
 
+class Contacts extends Component {
+  render() {
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          return (
+            <React.Fragment>
+              {value.contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
